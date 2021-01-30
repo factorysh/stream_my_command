3bbe770008a506f7fda092c7ef23368f3546d9dc
Mathieu Lecarme <mlecarme@bearstech.com>
2021-01-30 12:50:51 +0100

    Test with a large input. It fails.

--- stream/pubsub_test.go
+++ stream/pubsub_test.go
@@ -31,21 +31,37 @@ func TestOnePublisher(t *testing.T) {
 	assert.NoError(t, err)
 	defer os.Remove(p.buffer.Name())
 
-	wg := &sync.WaitGroup{}
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
+	READERS := 10
+	BLOBS := 10
 
+	wg := &sync.WaitGroup{}
+	wg.Add(READERS * BLOBS)
+
+	rs := make([]*mockWriter, READERS)
+	for i := 0; i < READERS; i++ {
 		ctx := context.TODO()
-		err = p.Subscribe(ctx, 0, &mockWriter{
+		r := &mockWriter{
 			b:  &bytes.Buffer{},
 			wg: wg,
 			i:  i,
-		})
+		}
+		err = p.Subscribe(ctx, 0, r)
 		assert.NoError(t, err)
+		rs[i] = r
 		fmt.Println("subscriber", i)
 	}
-	_, err = p.Write([]byte("beuha"))
-	assert.NoError(t, err)
-	fmt.Println("Wrote")
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for i := 0; i < BLOBS; i++ {
+		buff := make([]byte, 1024*1024)
+		_, err := r.Read(buff)
+		assert.NoError(t, err)
+		_, err = p.Write(buff)
+		assert.NoError(t, err)
+		fmt.Println("Wrote")
+	}
 	wg.Wait()
+	for _, r := range rs {
+		assert.Equal(t, BLOBS*1024*1024, r.b.Len())
+	}
+
 }
