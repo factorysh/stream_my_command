8902d1bf4b7db5713360990bb625bf9ed2a63516
Mathieu Lecarme <mlecarme@bearstech.com>
2021-02-01 14:28:19 +0100

    New package. Close. Env.

--- command/command.go
+++ command/command.go
@@ -1,7 +1,8 @@
-package stream
+package command
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
 	"os/exec"
@@ -18,12 +19,19 @@ func New() *Pool {
 	}
 }
 
-func (p *Pool) Command(ctx context.Context, out io.WriteCloser, name string, args ...string) error {
+func (p *Pool) Command(ctx context.Context, out io.WriteCloser, env map[string]string, name string, args ...string) error {
 	p.lock.Lock()
 	defer p.lock.Unlock()
+	defer out.Close()
 	cmd := exec.CommandContext(ctx, name, args...)
 	cmd.Stdout = out
 	cmd.Stderr = os.Stderr
+	envs := make([]string, 0)
+	for k, v := range env {
+		// FIXME assert k ~=[a-zA-Z_][a-zA-Z_0-9]+
+		envs = append(envs, fmt.Sprintf("%s=%s", k, v))
+	}
+	cmd.Env = envs
 	err := cmd.Start()
 	if err != nil {
 		return err
