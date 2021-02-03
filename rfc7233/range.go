2ffbfae185e67e6bdfdb330a798150b21464a8ae
Mathieu Lecarme <mlecarme@bearstech.com>
2021-02-03 19:00:54 +0100

    Test range.

--- rfc7233/range.go
+++ rfc7233/range.go
@@ -2,6 +2,7 @@ package rfc7233
 
 import (
 	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
@@ -37,9 +38,12 @@ func parseRange(raw string) (*Range, error) {
 	return rang, nil
 }
 
-type Ranges []*Range
+type Ranges struct {
+	ranges []*Range
+	r      int
+}
 
-func Parse(raw string) (Ranges, error) {
+func Parse(raw string) (*Ranges, error) {
 	if !strings.HasPrefix(raw, "bytes=") {
 		return nil, fmt.Errorf("It doesn't start with bytes= : %s", raw)
 	}
@@ -47,13 +51,38 @@ func Parse(raw string) (Ranges, error) {
 	if len(r) == 0 {
 		return nil, fmt.Errorf("Empty range %s", raw)
 	}
-	ranges := make(Ranges, len(r))
+	ranges := &Ranges{
+		ranges: make([]*Range, len(r)),
+	}
 	for i := 0; i < len(r); i++ {
 		var err error
-		ranges[i], err = parseRange(r[i])
+		ranges.ranges[i], err = parseRange(r[i])
 		if err != nil {
 			return nil, err
 		}
 	}
 	return ranges, nil
+}
+
+func (r *Ranges) Len() int {
+	return len(r.ranges)
+}
+
+func (r *Ranges) Next() (start, end int, infinite bool, err error) {
+	if r.r == len(r.ranges) {
+		return 0, 0, false, io.EOF
+	}
+	rr := r.ranges[r.r]
+	if !rr.start {
+		start = 0
+	} else {
+		start = rr.min
+	}
+	if !rr.end {
+		infinite = true
+	} else {
+		end = rr.max
+	}
+	r.r++
+	return start, end, infinite, nil
 }
--- rfc7233/range_test.go
+++ rfc7233/range_test.go
@@ -2,6 +2,7 @@ package rfc7233
 
 import (
 	"fmt"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,5 +12,35 @@ func TestRange(t *testing.T) {
 	r, err := Parse("bytes=500-600,601-999,1001-")
 	assert.NoError(t, err)
 	fmt.Println(r)
-	assert.Len(t, r, 3)
+	assert.Equal(t, 3, r.Len())
+}
+
+func TestNext(t *testing.T) {
+	r, err := Parse("bytes=1-3,5-")
+	assert.NoError(t, err)
+	values := make([]int, 10)
+	for i := 0; i < len(values); i++ {
+		values[i] = i
+	}
+	stack := make([]int, 0)
+	for true {
+		start, end, infinite, err := r.Next()
+		if err != nil {
+			if err != io.EOF {
+				assert.NoError(t, err)
+			}
+			break
+		}
+		var sub []int
+		if infinite {
+			sub = values[start:]
+		} else {
+			sub = values[start : end+1]
+		}
+		for _, v := range sub {
+			stack = append(stack, v)
+		}
+	}
+	assert.Equal(t, []int{1, 2, 3, 5, 6, 7, 8, 9}, stack)
+
 }
