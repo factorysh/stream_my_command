fbc73e3ff14acecba3bd2479d7dedadb510d7b69
Mathieu Lecarme <mlecarme@bearstech.com>
2021-02-03 22:45:49 +0100

    Simple test

--- stream/longbuffer_test.go
+++ stream/longbuffer_test.go
@@ -14,6 +14,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSimple(t *testing.T) {
+	l, err := NewLongBuffer(os.TempDir())
+	assert.NoError(t, err)
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	buff := make([]byte, 3*1024*1024)
+	h1 := sha256.New()
+	for i := 0; i < 10; i++ {
+		_, _ = r.Read(buff)
+		n, err := l.Write(buff)
+		assert.NoError(t, err)
+		_, err = h1.Write(buff)
+		assert.NoError(t, err)
+		assert.Equal(t, 3*1024*1024, n)
+		fmt.Println("Write", i)
+		time.Sleep(time.Duration(r.Int63n(10)) * time.Millisecond)
+	}
+	buff = make([]byte, 512)
+	_, _ = r.Read(buff)
+	w, err := l.Write(buff)
+	assert.NoError(t, err)
+	_, err = h1.Write(buff)
+	assert.NoError(t, err)
+	assert.Equal(t, 512, w)
+
+	assert.Equal(t, 30*1024*1024+512, l.Len())
+	l.Close()
+	reader := l.Reader(0)
+	b := new(bytes.Buffer)
+	n, err := io.Copy(b, reader)
+	assert.NoError(t, err)
+	fmt.Println("path", l.path)
+	assert.Equal(t, int64(30*1024*1024+512), n)
+	h2 := sha256.New()
+	h2.Write(b.Bytes())
+	assert.Equal(t, h1.Sum(nil), l.Hash())
+}
+
 func TestLongBuffer(t *testing.T) {
 	l, err := NewLongBuffer(os.TempDir())
 	assert.NoError(t, err)
